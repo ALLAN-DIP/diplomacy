83fa8f8d8a1fff1ea7e7f72d0c3d2d25c6d5c434
Alex Hedges <git@alexhedges.dev>
2025-02-18 18:51:05 -0500

    Make `convert_svg_maps_to_react.sh` directory-agnostic

--- diplomacy/web/convert_svg_maps_to_react.sh
+++ diplomacy/web/convert_svg_maps_to_react.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+
+set -euxo pipefail
+
+SCRIPT_FILE=$(realpath "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT_FILE")
+cd "$SCRIPT_DIR"
+
 python svg_to_react.py --input src/diplomacy/maps/svg/standard.svg --output src/gui/maps/standard/ --name SvgStandard
 python svg_to_react.py --input src/diplomacy/maps/svg/ancmed.svg --output src/gui/maps/ancmed/ --name SvgAncMed
 python svg_to_react.py --input src/diplomacy/maps/svg/modern.svg --output src/gui/maps/modern/ --name SvgModern
