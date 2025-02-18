abfd937244af487cb56c148677feab0b6399dcca
Feng Gu <fenggu2000@gmail.com>
2025-02-17 20:49:17 -0500

    stance switch

--- diplomacy/web/src/gui/pages/content_game.jsx
+++ diplomacy/web/src/gui/pages/content_game.jsx
@@ -55,6 +55,7 @@ import { default as Tabs2 } from "@mui/material/Tabs";
 import { default as Tab2 } from "@mui/material/Tab";
 import Box from "@mui/material/Box";
 import Badge from "@mui/material/Badge";
+import Switch from "@mui/material/Switch";
 
 import {
     MainContainer,
@@ -971,7 +972,10 @@ export class ContentGame extends React.Component {
             .then(() => {
                 page.success("Game processed.");
                 this.props.data.clearInitialOrders();
-                return this.setState({ hasInitialOrders: false, hoverOrders: [] });
+                return this.setState({
+                    hasInitialOrders: false,
+                    hoverOrders: [],
+                });
             })
             .catch((err) => {
                 page.error(err.toString());
@@ -1098,7 +1102,7 @@ export class ContentGame extends React.Component {
             if (!UTILS.javascript.count(orders[powerName]))
                 orders[powerName] = null;
             this.__store_orders(orders);
-            await this.setState({ orders: orders, hoverOrders: [], });
+            await this.setState({ orders: orders, hoverOrders: [] });
         }
         this.setOrders();
     }
@@ -1119,7 +1123,7 @@ export class ContentGame extends React.Component {
             this.sendOrderLog(engine.client, "clear", null);
             allOrders[currentPowerName] = null;
             this.__store_orders(allOrders);
-            await this.setState({ orders: allOrders, hoverOrders: [], });
+            await this.setState({ orders: allOrders, hoverOrders: [] });
         }
         this.setOrders();
     }
@@ -1879,9 +1883,16 @@ export class ContentGame extends React.Component {
             <Conversation
                 style={{ minWidth: "200px" }}
                 info={
-                    isAdmin && protagonist !== "GLOBAL"
-                        ? engine.powers[protagonist].getController()
-                        : ""
+                    isAdmin && protagonist !== "GLOBAL" ? (
+                        engine.powers[protagonist].getController()
+                    ) : (
+                        <div>
+                            friendly?
+                            <Switch color="success" size="small"
+                            onChange={() => {}}
+                            ></Switch>
+                        </div>
+                    )
                 }
                 className={
                     protagonist === currentTabId
@@ -3106,7 +3117,8 @@ export class ContentGame extends React.Component {
 
         return (
             <div className={"col-2 mb-4"}>
-                <div>Get
+                <div>
+                    Get
                     <select name={"stance"} id={"stance"}>
                         <option value={"F"}>friendly</option>
                         <option value={"H"}>hostile</option>
@@ -3117,21 +3129,27 @@ export class ContentGame extends React.Component {
                             <option value={tabName}>{tabName}</option>
                         ))}
                     </select>
-                    <Button 
+                    <Button
                         title={UTILS.html.UNICODE_RIGHT_ARROW}
                         color={"primary"}
                         onClick={() => {
-                            console.log(`${document.getElementById("toPower").value} with stance ${document.getElementById("stance").value}`);
-                            this.sendLogData(engine.client, `STANCE:${document.getElementById("stance").value}:${document.getElementById("toPower").value}`);
+                            console.log(
+                                `${
+                                    document.getElementById("toPower").value
+                                } with stance ${
+                                    document.getElementById("stance").value
+                                }`
+                            );
+                            this.sendLogData(
+                                engine.client,
+                                `STANCE:${
+                                    document.getElementById("stance").value
+                                }:${document.getElementById("toPower").value}`
+                            );
                         }}
-                    >
-                    </Button>
+                    ></Button>
                 </div>
-                {suggestionType === null && (
-                    <div>
-                        No advice for this turn
-                    </div>
-                )}
+                {suggestionType === null && <div>No advice for this turn</div>}
                 {suggestionType !== null && suggestionType === 0 && (
                     <div>You are on your own</div>
                 )}
