42edac52e14bb542c462197b1056f5903104bfb5
wwongkamjan <wwongkam@terpmail.umd.edu>
2025-03-04 22:55:58 -0500

    add deception suggestion type

--- diplomacy/utils/constants.py
+++ diplomacy/utils/constants.py
@@ -69,6 +69,7 @@ class SuggestionType(IntFlag):
     MOVE = 2
     COMMENTARY = 4
     OPPONENT_MOVE = 8
+    DECEPTION = 16
 
     # Old aliases for backwards compatibility
     MESSAGE_ONLY = MESSAGE
