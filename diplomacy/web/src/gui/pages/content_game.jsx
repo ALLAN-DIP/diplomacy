fa9d3d26f0411167bd0f531b38ab9543d0ca20aa
Alex Hedges <git@alexhedges.dev>
2025-03-19 17:14:10 -0400

    Remove duplicate `hoverOrders` property

--- diplomacy/web/src/gui/pages/content_game.jsx
+++ diplomacy/web/src/gui/pages/content_game.jsx
@@ -539,7 +539,6 @@ export class ContentGame extends React.Component {
                     this.reloadDeadlineTimer(networkGame);
                     return this.setState({
                         orders: null,
-                        hoverOrders: [],
                         messageHighlights: {},
                         orderBuildingPath: [],
                         orderDistribution: [],
