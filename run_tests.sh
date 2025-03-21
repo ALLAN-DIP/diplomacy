6a5107dd5a27f12cda32d5fb7f34815468251fb7
Alex Hedges <git@alexhedges.dev>
2025-03-21 18:41:11 -0400

    Move `eslint` into `Makefile`
    `run_tests.sh` overcomplicates this test by running multiple checks.
    Everything is already installed by `make install`, so we can just assume
    it exists!

--- run_tests.sh
+++ run_tests.sh
@@ -4,26 +4,6 @@ export PYTHONIOENCODING=utf-8
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 FAILED=0
 
-# Running eslint
-echo ""
-echo "------------------------------"
-echo "         ESLINT TESTS         "
-echo "------------------------------"
-echo ""
-if [ -f "$DIR/diplomacy/web/node_modules/.bin/eslint" ]; then
-    if [ -z ${NVM_DIR+x} ]; then
-        export NVM_DIR="$HOME/.nvm"
-        [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-        [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-    fi
-    cd $DIR/diplomacy/web/
-    node_modules/.bin/eslint --ext js,jsx . || FAILED=1
-    npm run build || FAILED=1
-    cd -
-else
-    echo "Skipping ESLint. Make sure NVM and NodeJS are installed first."
-fi
-
 # Exiting
 if [[ "$FAILED" -eq 1 ]]; then
     echo "*** TESTS FAILED ***"
