bc82fef1d6d02f6c86d3c78b4665e2e598770cf5
Alex Hedges <git@alexhedges.dev>
2025-03-21 18:35:01 -0400

    Move `pytest` running into `Makefile`
    As part of this commit, I removed the multiple options for running
    `pytest` in `run_tests.sh`.

--- run_tests.sh
+++ run_tests.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 # Syntax: ./run_tests               -- Run tests in parallel across CPUs
-#         ./run_tests <nb_cores>    -- Run tests in parallel across this number of CPUs
-#         ./run_tests 0             -- Only runs the pylint tests
 export PYTHONIOENCODING=utf-8
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 FAILED=0
-
-# Running pytest
-if [ "${1:-auto}" != "0" ]; then
-    pytest -v --forked -n "${1:-auto}" diplomacy || FAILED=1
-fi
 
 # Running pylint
 echo ""
