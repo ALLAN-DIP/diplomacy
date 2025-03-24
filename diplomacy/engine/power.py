8226af70286d43fde42e9a8de3cd708d008e2745
Feng Gu <fenggu2000@gmail.com>
2025-03-24 13:19:30 -0400

    remove order is set for wait()

--- diplomacy/engine/power.py
+++ diplomacy/engine/power.py
@@ -360,7 +360,7 @@ class Power(Jsonable):
         """ (Network Method) Return True if this power does not wait
             (ie. if we could already process orders of this power).
         """
-        return self.order_is_set and not self.wait
+        return not self.wait
 
     def update_controller(self, username, timestamp):
         """ (Network Method) Update controller with given username and timestamp. """
