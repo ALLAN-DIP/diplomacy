6bb52fd9502ba3e68e1a770a7bca1ecb27e4f848
Alex Hedges <git@alexhedges.dev>
2025-04-11 18:36:49 -0400

    Reformat generated map files with Prettier
    This should make diffs _much_ easier to read!

--- diplomacy/web/convert_svg_maps_to_react.sh
+++ diplomacy/web/convert_svg_maps_to_react.sh
@@ -10,3 +10,5 @@ python svg_to_react.py --input src/diplomacy/maps/svg/standard.svg --output src/
 python svg_to_react.py --input src/diplomacy/maps/svg/ancmed.svg --output src/gui/maps/ancmed/ --name SvgAncMed
 python svg_to_react.py --input src/diplomacy/maps/svg/modern.svg --output src/gui/maps/modern/ --name SvgModern
 python svg_to_react.py --input src/diplomacy/maps/svg/pure.svg --output src/gui/maps/pure/ --name SvgPure
+
+pre-commit run prettier --files src/gui/maps/{ancmed,modern,pure,standard}/* &>/dev/null || true
