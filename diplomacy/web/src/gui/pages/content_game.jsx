d0d299c00d3f7eb0bb14e71e6b592ba2784e4733
Alex Hedges <git@alexhedges.dev>
2025-04-30 17:15:42 -0400

    Remove always-present scrollbar unless needed
    This commit adjusts `overflow-y`[^1] to only display the scrollbar for
    the order entry form when it is needed. Most of the time, the component
    can fit within this space, resulting an awkward-looking, disabled
    scrollbar on the right side.
    
    [^1]: https://developer.mozilla.org/en-US/docs/Web/CSS/overflow-y

--- diplomacy/web/src/gui/pages/content_game.jsx
+++ diplomacy/web/src/gui/pages/content_game.jsx
@@ -3202,7 +3202,7 @@ export class ContentGame extends React.Component {
                         {/* Orders. */}
                         <div
                             className={"panel-orders mb-4"}
-                            style={{ maxHeight: "500px", overflowY: "scroll" }}
+                            style={{ maxHeight: "500px", overflowY: "auto" }}
                         >
                             {currentTabOrderCreation ? (
                                 <div className="mb-4">
