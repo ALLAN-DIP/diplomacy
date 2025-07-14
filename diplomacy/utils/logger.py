a3f1b98e75e97e27596a12ecf61e41237cd894f4
Alex Hedges <git@alexhedges.dev>
2025-07-14 17:27:29 -0400

    Use lowercase variable names in function
    This change is separate from the previous commit to make the code
    movement easier to see.

--- diplomacy/utils/logger.py
+++ diplomacy/utils/logger.py
@@ -6,29 +6,29 @@ import os
 
 def initialize_logging() -> None:
     # Defining root logger
-    ROOT = logging.getLogger("diplomacy")
-    ROOT.setLevel(logging.DEBUG)
-    ROOT.propagate = False
+    root = logging.getLogger("diplomacy")
+    root.setLevel(logging.DEBUG)
+    root.propagate = False
 
-    STREAM_HANDLER = logging.StreamHandler()
-    STREAM_HANDLER.setLevel(logging.DEBUG)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.DEBUG)
 
     # Monkey patch module to show milliseconds
     logging.Formatter.default_msec_format = "%s.%03d"
 
-    FORMATTER = logging.Formatter(
+    formatter = logging.Formatter(
         fmt="[%(asctime)s] [%(levelname)s] [%(name)s[%(process)d]] %(message)s"
     )
-    STREAM_HANDLER.setFormatter(FORMATTER)
-    ROOT.addHandler(STREAM_HANDLER)
+    stream_handler.setFormatter(formatter)
+    root.addHandler(stream_handler)
 
     if "DIPLOMACY_LOG_FILE" in os.environ:
-        LOG_FILE_NAME = os.environ["DIPLOMACY_LOG_FILE"]
-        ROOT.info("Logging into file: %s", LOG_FILE_NAME)
-        FILE_HANDLER = logging.FileHandler(LOG_FILE_NAME)
-        FILE_HANDLER.setLevel(logging.DEBUG)
-        LOG_FILE_FORMATTER = logging.Formatter(
+        log_file_name = os.environ["DIPLOMACY_LOG_FILE"]
+        root.info("Logging into file: %s", log_file_name)
+        file_handler = logging.FileHandler(log_file_name)
+        file_handler.setLevel(logging.DEBUG)
+        log_file_formatter = logging.Formatter(
             fmt="%(asctime)s %(name)s[%(process)d] %(levelname)s %(message)s"
         )
-        FILE_HANDLER.setFormatter(LOG_FILE_FORMATTER)
-        ROOT.addHandler(FILE_HANDLER)
+        file_handler.setFormatter(log_file_formatter)
+        root.addHandler(file_handler)
